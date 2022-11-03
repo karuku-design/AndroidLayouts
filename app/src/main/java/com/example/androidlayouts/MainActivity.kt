9510933a83f86ee236a6f9a3aa542f311abf3ca0
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-03 11:26:10 +0300

    Added code to find the UI elements on MainActivity.kt.

--- app/src/main/java/com/example/androidlayouts/MainActivity.kt
+++ app/src/main/java/com/example/androidlayouts/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.linearlayout2)
+
+        val edtTxt=findViewById<EditText>(R.id.editText1)
+        val submitBtn=findViewById<Button>(R.id.btn1)
     }
 }
